4fa719efad9696aec7b9ec5537cad65d8c9406e0
Kal <amjsimpson2013@gmail.com>
2024-06-04 12:48:59 -0400

    started facebook integration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { AppModule } from './app/app.module';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+window.fbAsyncInit = function() {
+  FB.init({
+    appId            : '1138139490730894',
+    xfbml            : true,
+    version          : 'v20.0'
+  });
+};
+
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
